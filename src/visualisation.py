75304903adf6a4f9f03f265323ee4ea174bee9cf
Alam <seif.johanna@gmail.com>
2020-07-01 17:57:36 +0200

    Visualisation of the data

---
+++ src/visualisation.py
@@ -0,0 +1,33 @@
+from process_data import load_and_process_data
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.close("all")
+
+# Loading preprocessed data
+dataset = load_and_process_data('../data/train.csv')
+
+##############################################
+#           Data visualisation               #
+##############################################
+
+print(dataset.info())
+
+# Data description
+print("\nDescription of the numerical features:")
+print(dataset.describe())
+
+# Filling age
+dataset["Age"].fillna(value=dataset["Age"].median(), inplace=True)
+print("\nDescription of the numerical features after filling the age:")
+print(dataset.describe())
+
+# Set seaborn style
+sns.set(style="ticks")
+fg = sns.FacetGrid(data=dataset[dataset['Fare'] < 300], hue='Survived', col='Pclass', row='Parch', aspect=1)
+fg.map(plt.scatter, 'Age', 'Fare').add_legend()
+plt.show()
+
+# TODO Classification taking taking off Id, Name, SibSp, Tiket, Cabin, Embarked
