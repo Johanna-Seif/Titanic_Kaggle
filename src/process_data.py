702ede324fa61299a4621b46533e110c22bfd27d
Alam <seif.johanna@gmail.com>
2020-07-03 17:09:31 +0200

    Filling empty rows, removing features

--- src/process_data.py
+++ src/process_data.py
@@ -8,8 +8,32 @@ def load_and_process_data(file_path):
     data = pd.read_csv(file_path, header=0)
     print('Data loaded.')
 
+    # Processing data
+    # Taking off PassengerId, Name, Embarked, Ticket
+    del data['Name']
+    del data['Ticket']
+    del data['Embarked']
+    # Taking off SibSp to avoid redundancy
+    del data['SibSp']
+    # Taking of Cabin because too sparse
+    del data['Cabin']
+
+    # Filling age and fare with the median
+    data['Age'].fillna(value=data['Age'].median(), inplace=True)
+    data['Fare'].fillna(value=data['Fare'].median(), inplace=True)
+
+    # Changing string to int in Sex
+    mymap = {'female' : 1, 'male' : 2}
+    data = data.applymap(lambda s: mymap.get(s) if s in mymap else s)
+
+    print('Data processed.')
+
     return data
 
 
-def prediction_to_csv(output_array, file_path):
+def prediction_to_csv(id, prediction, file_path):
+    # output_array =
+    prediction = prediction.reshape((prediction.shape[0], 1))
+    id = id.to_numpy().reshape((prediction.shape[0], 1))
+    output_array = np.concatenate((id, prediction), axis=1)
     np.savetxt(file_path, output_array, fmt='%i', delimiter=',', header='PassengerId,Survived', comments='')
